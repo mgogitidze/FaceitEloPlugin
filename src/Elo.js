43664b5360cd805cdcb482ae817a3d2b0f0ae089
Maksim Gogitidze <maksim.gogitidze@rt.ru>
2024-08-10 23:02:07 +0300

    feat: add top fetcher + refactor

---
+++ src/Elo.js
@@ -0,0 +1,11 @@
+import './Elo.css';
+import React from "react";
+
+export default function Elo(props) {
+
+    return <div id="elo-wrapper">
+        <div id="elo">{props.elo} </div>
+        <div id="elop">ELO</div>
+    </div>
+}
+
