dc3bef8f9c9d19111504a41f5d84d576987ba1fb
Maksim Gogitidze <maksim.gogitidze@rt.ru>
2024-10-18 16:35:22 +0300

    fix: styles

--- src/components/StatsElement.js
+++ src/components/StatsElement.js
@@ -1,7 +1,7 @@
 function StatsElement(props) {
     return <div className="ds">
-        <div className="ds_element">{props.value}</div>
-        <div className="ds_element">{props.name}</div>
+        <div className="ds_element_first">{props.value}</div>
+        <div className="ds_element_first">{props.name}</div>
     </div>
 }
 
